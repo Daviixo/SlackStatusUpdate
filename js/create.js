9ea193bd97b60c27a884f3e9fb6fec20b01083c5
David Diaz <daviidvx93@gmail.com>
2022-06-09 12:56:22 -0600

    Timer removed

--- js/create.js
+++ js/create.js
@@ -11,139 +11,6 @@ function createTemplate() {
     document.getElementById("actionsTaken").innerHTML = localStorage.getItem("myActions");
     document.getElementById("doing").innerHTML = localStorage.getItem("myDoing");
 
-    // Timer stuff
-    // Credit: Mateusz Rybczonec
-
-    const FULL_DASH_ARRAY = 283;
-    const WARNING_THRESHOLD = 10;
-    const ALERT_THRESHOLD = 5;
-
-    const COLOR_CODES = {
-    info: {
-        color: "green"
-    },
-    warning: {
-        color: "orange",
-        threshold: WARNING_THRESHOLD
-    },
-    alert: {
-        color: "red",
-        threshold: ALERT_THRESHOLD
-    }
-    };
-
-    //We can adjust the time here
-
-    let TIME_LIMIT = 5;
-    let timePassed = 0;
-    let timeLeft = TIME_LIMIT;
-    let timerInterval = null;
-    let remainingPathColor = COLOR_CODES.info.color;
-
-    document.getElementById("app").innerHTML = `
-    <div class="base-timer">
-    <svg class="base-timer__svg" viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
-        <g class="base-timer__circle">
-        <circle class="base-timer__path-elapsed" cx="50" cy="50" r="45"></circle>
-        <path
-            id="base-timer-path-remaining"
-            stroke-dasharray="283"
-            class="base-timer__path-remaining ${remainingPathColor}"
-            d="
-            M 50, 50
-            m -45, 0
-            a 45,45 0 1,0 90,0
-            a 45,45 0 1,0 -90,0
-            "
-        ></path>
-        </g>
-    </svg>
-    <span id="base-timer-label" class="base-timer__label">${formatTime(
-        timeLeft
-    )}</span>
-    </div>
-    `;
-
-    buttonClick.addEventListener("click", () => {
-        
-
-        console.log("Button was clicked. REFRESHING!");
-        // localStorage.setItem("mySelect", "");
-        // localStorage.setItem("myValue", "");
-        // localStorage.setItem("myImpact", "");
-        // localStorage.setItem("myActions", "");
-        // localStorage.setItem("myDoing", "");
-        location.reload();
-
-    });
-
-    startTimer();
-
-    function onTimesUp() {
-    clearInterval(timerInterval);
-    }
-
-    function startTimer() {
-    timerInterval = setInterval(() => {
-        timePassed = timePassed += 1;
-        timeLeft = TIME_LIMIT - timePassed;
-        document.getElementById("base-timer-label").innerHTML = formatTime(
-        timeLeft
-        );
-        setCircleDasharray();
-        setRemainingPathColor(timeLeft);
-
-        if (timeLeft === 0) {
-        onTimesUp();
-        alert("Times up!");
-        }
-    }, 1000);
-}
-
-    function formatTime(time) { 
-    const minutes = Math.floor(time / 60);
-    let seconds = time % 60;
-
-    if (seconds < 10) {
-        seconds = `0${seconds}`;
-    }
-
-    return `${minutes}:${seconds}`;
-    }
-
-    function setRemainingPathColor(timeLeft) {
-    const { alert, warning, info } = COLOR_CODES;
-    if (timeLeft <= alert.threshold) {
-        document
-        .getElementById("base-timer-path-remaining")
-        .classList.remove(warning.color);
-        document
-        .getElementById("base-timer-path-remaining")
-        .classList.add(alert.color);
-    } else if (timeLeft <= warning.threshold) {
-        document
-        .getElementById("base-timer-path-remaining")
-        .classList.remove(info.color);
-        document
-        .getElementById("base-timer-path-remaining")
-        .classList.add(warning.color);
-    }
-    }
-
-    function calculateTimeFraction() {
-    const rawTimeFraction = timeLeft / TIME_LIMIT;
-    return rawTimeFraction - (1 / TIME_LIMIT) * (1 - rawTimeFraction);
-    }
-
-    function setCircleDasharray() {
-    const circleDasharray = `${(
-        calculateTimeFraction() * FULL_DASH_ARRAY
-    ).toFixed(0)} 283`;
-    document
-        .getElementById("base-timer-path-remaining")
-        .setAttribute("stroke-dasharray", circleDasharray);
-    }
-
     // End of the timer stuff - Copyright and special thanks to Geoff Graham https://codepen.io/geoffgraham/pen/yLywVbW 
 
     //Our final form should show:
@@ -195,14 +62,12 @@ function createTemplate() {
     winPrint.document.write('<title>Slack Status Update</title><strong>Current Status Update#' + value + '</strong>' +
     '<br><br><strong>Impact:</strong><br>' + cImpact + '<br><br><strong>What have we done so far?</strong><br>' + cActions +
     '<br><br><strong>What are we currently doing?</strong><br>' + cDoing +
-    '<br><br><br>/remind @channel “This is a test 2” in 5 seconds'
+    '<br><br><br>/remind @noc-team "Time to post another slack update" in 60 minutes'
     
     );
 
 
 }
-
-// '<br><br><br><strong>/remind @noc-team "Time to post another slack update" in 60 minutes</strong>'
 
 function pageLoads(){
 
